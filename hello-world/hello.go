9b5e9c8b39d7e11b6807b07c1a35532e986157bf
Nobuchika Tanaka <codequokka@gmail.com>
2024-07-15 08:03:37 +0900

    Add langugae arg to Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, langugae string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if langugae == spanish {
+		return spanishHelloPrefix + name
+	}
+	if langugae == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
